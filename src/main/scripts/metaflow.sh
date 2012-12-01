2a7302120506189a5b2e842a789eddc8efb40e68
markusgeipel <markusgeipel@f2a00648-b9af-4511-a9bd-0c59966c402f>
2012-12-01 10:05:50 +0000

    switched to new Metaflow&Metamorph SNAPSHOT
    git-svn-id: https://svn.code.sf.net/p/culturegraph/code/metaflow/trunk@1685 f2a00648-b9af-4511-a9bd-0c59966c402f

--- src/main/scripts/metaflow.sh
+++ src/main/scripts/metaflow.sh
@@ -3,9 +3,9 @@
 dir=`dirname $0`
 
 if uname | grep -iq cygwin; then
-    java -jar `cygpath -am $dir/metaflow-0.0.7-SNAPSHOT.jar` $*
+    java -jar `cygpath -am $dir/metaflow-0.0.8-SNAPSHOT.jar` $*
 else
-    java -jar $dir/metaflow-0.0.7-SNAPSHOT.jar $*
+    java -jar $dir/metaflow-0.0.8-SNAPSHOT.jar $*
 fi
 
 
